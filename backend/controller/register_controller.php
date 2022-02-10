4e92e602d8b099c02a26cb59639078d9de43f71f
tanguy <tlegalldutertre@gmail.com>
2022-02-10 14:31:15 +0100

    avec hash password

--- backend/controller/register_controller.php
+++ backend/controller/register_controller.php
@@ -1,5 +1,6 @@
 <?php
 include __DIR__ . '/../Database/DB.php';
+include __DIR__ . '/../utilitaire/hash.php';
 
 class register_controller
 {
@@ -9,30 +10,40 @@ class register_controller
     protected $dateDeNaissance;
     protected $motDePass;
     protected $connection_valid;
-    
+    protected $password_hash;
     public function __construct()
     {
+
         
-        // $this->pseudo = $_POST["peudo"];
-        // $this->password = $_POST["birthdate"];
-        // $this->email = $_POST["email"];
-        // $this->dateDeNaissance = $_POST["birthdate"];
-        // $this->motDePass = $_POST["password"];
-        // $this->connection = $_POST["connection"];
+        $this->pseudo = $_POST["peudo"];
+        $this->email = $_POST["email"];
+        $this->motDePass = $_POST["password"]; 
+        $this->dateDeNaissance = $_POST["birthdate"];
         
+        $this->connection = $_POST["connection"];
+        
+        
+    }   
+    
+    
+    public function hash() {
+        
+        $hash = new hash();
+        $this->password_hash = $hash->hash($this->motDePass);
         
     }
-    
     
     public function insert() {
         $register = new DB();
         // check email
         $check = $register->get_email("SELECT * from users WHERE email like 'ala@gmail.com'");
-print_r($check);
+        
+        
         if (!empty($check)) {
             echo "Votre email est déja pris";
         } else {
             echo "all good";
+            
             
             $register->insert("INSERT INTO users(pseudo,email,password,birthdate,userpp,banner,description,theme,user_date) VALUES('dsfsdfsdf','sqldka@gmail.com','asdqsdq','2018-09-24','dfssdf','erzzz','sdjklkj','8','2018-09-24')");
         }
@@ -41,12 +52,8 @@ print_r($check);
 }
 
 
-
-
-// exécution
-
 $register_controller = new register_controller();
-
+$register_controller->hash();
 $register_controller->insert();
 
 print_r($register_controller);
