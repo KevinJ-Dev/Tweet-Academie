fab8c98854789e9b714055f8297e11d31332cbd4
fabien Le Fichoux <fabien.le-fichoux@epitech.eu>
2022-02-10 16:36:48 +0100

    Post onclick register

---
+++ frontend/js/scriptAJAX.js
@@ -0,0 +1,13 @@
+$("#create").click(function () {
+    $.ajax({
+        url: 'frontend/view/register.php',
+        type: 'POST'
+    });
+});
+
+$("#create-connect").click(function () {
+    $.ajax({
+        url: 'frontend/view/login.php',
+        type: 'POST'
+    });
+});
