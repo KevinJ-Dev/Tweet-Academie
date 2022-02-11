49d3c123a4acf33c8ef4e4676bf7f5022f7d6feb
tanguy <tlegalldutertre@gmail.com>
2022-02-11 14:17:41 +0100

    ajax dans le register view

--- backend/controller/register_controller.php
+++ backend/controller/register_controller.php
@@ -1,7 +1,7 @@
 <?php
 include __DIR__ . '/../Database/DB.php';
 include __DIR__ . '/../utilitaire/hash.php';
-
+include "../Tweet-Academie/backend/utilitaire/hash.php";
 class register_controller
 {
     protected $pseudo;
@@ -11,18 +11,17 @@ class register_controller
     protected $motDePass;
     protected $connection_valid;
     protected $password_hash;
+    
+    
+    
     public function __construct()
-    {
-
-        
+    { 
         $this->pseudo = $_POST["peudo"];
         $this->email = $_POST["email"];
         $this->motDePass = $_POST["password"]; 
         $this->dateDeNaissance = $_POST["birthdate"];
         
-        $this->connection = $_POST["connection"];
-        
-        
+        $this->connection = $_POST["connection"];   
     }   
     
     
@@ -36,24 +35,31 @@ class register_controller
     public function insert() {
         $register = new DB();
         // check email
-        $check = $register->get_email("SELECT * from users WHERE email like 'ala@gmail.com'");
+        $check = $register->get_email("SELECT * from users WHERE email like 'sqldka@gmail.com'");
         
-        
-        if (!empty($check)) {
-            echo "Votre email est déja pris";
-        } else {
-            echo "all good";
-            
-            
-            $register->insert("INSERT INTO users(pseudo,email,password,birthdate,userpp,banner,description,theme,user_date) VALUES('dsfsdfsdf','sqldka@gmail.com','asdqsdq','2018-09-24','dfssdf','erzzz','sdjklkj','8','2018-09-24')");
+        $test = $register->get_all("SELECT * from users WHERE email like 'sqldka@gmail.com'");
+        foreach ($test as $val) {
+            echo $val['password'];
+
+          
         }
+            
+            if (empty($check)) {
+                echo "all-good";
+                $register->insert("INSERT INTO users(pseudo,email,password,birthdate,userpp,banner,description,theme,user_date) VALUES('dsfsdfsdf','sqldka@gmail.com','asdqsdq','2018-09-24','dfssdf','erzzz','sdjklkj','8','2018-09-24')");
+                
+            } else {
+                echo "Votre mail est déjà utilisé";
+                
+                
+            }
+        }
+        
     }
     
-}
-
-
-$register_controller = new register_controller();
-$register_controller->hash();
-$register_controller->insert();
-
-print_r($register_controller);
+    
+    $register_controller = new register_controller();
+    $register_controller->hash();
+    $register_controller->insert();
+    
+    print_r($register_controller);
