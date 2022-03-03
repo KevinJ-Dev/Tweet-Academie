63ff0e50f3897586a4c244b7dab2057ee58f8102
Kévin <kevin.jourdain@epitech.eu>
2022-03-03 11:34:14 +0100

    Navbar vertical

---
+++ frontend/view/app/left_view.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="/frontend/css/root.css">
+<div>
+    <img src="/frontend/img/logo.png" alt="Logo Tweeter">
+    <nav class="navbar navbar-light bg-info">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Acceuil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Messages</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Profile</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Recherche</a>
+      </li>
+      <li class="nav-item">
+        <button>Tweeter</button>
+      </li>
+      <li class="nav-item">
+          <a href="?p=logout">Déconnexion</a>
+      </li>
+    </ul>
+  </div>
+</nav>
