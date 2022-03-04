ccd7bda90c92a6df43a06c67926d39f89779ee65
Kévin <kevin.jourdain@epitech.eu>
2022-03-04 11:34:31 +0100

    Navbar

--- frontend/view/app/left_view.php
+++ frontend/view/app/left_view.php
@@ -1,55 +1,59 @@
 <link rel="stylesheet" href="/frontend/css/root.css">
 <link rel="stylesheet" href="/frontend/css/home.css">
+<link rel="stylesheet" href="/frontend/js/left_view.js">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 <aside>
 <img class="logo" src="/frontend/img/logo.png" alt="logo">
     <nav>
-        <button class="home">
+        <button class="home active">
             <div>
                 <div class="icon">
-                  <!-- Icon -->
-                </div>Acceuil
+                <i class="fa-solid fa-house-chimney"></i>
+                </div>
+                <a class="link" href="?p=app">Acceuil</a>
+                <!-- Acceuil -->
             </div>
         </button>
         <button class="message">
             <div>
                 <div class="icon">
-                  <!-- Icon -->
+                <i class="fa-solid fa-envelope"></i>
                 </div>
-                Messages
+                <a class="link" href="#">Messages</a>
             </div>
         </button>
-        <button class="profil active">
+        <button class="profil">
             <div>
                 <div class="icon">
-                  <!-- Icon -->
+                <i class="fa-solid fa-user"></i>
                 </div>
-                Profile
+                <a class="link" href="?p=profil">Profile</a>
             </div>
         </button>
         <button class="search">
             <div>
                 <div class="icon">
-                  <!-- Icon -->
+                <i class="fa-solid fa-magnifying-glass"></i>
                     <div class="top">
                     </div>
                 </div>
-                Recherche
+                <a class="link" href="#">Recherche</a>
             </div>
         </button>
         <button class="twiter">
             <div>
                 <div class="icon">
-                  <!-- Icon -->
+                <i class="fa-brands fa-twitter"></i>
                 </div>
-                Tweeter
+                <a class="link" href="#">Tweeter</a>
             </div>
         </button>
         <button class="logout">
             <div>
                 <div class="icon">
-                  <!-- Icon -->
+                <i class="fa-solid fa-circle-xmark"></i>
                 </div>
-                <a href="?p=logout">Déconnection</a>
+                <a class="link" href="?p=logout">Déconnection</a>
             </div>
         </button>
     </nav>
